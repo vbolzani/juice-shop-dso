35df5cf8c7970eeab531d4afa879eec416504f26
vbolzani <vbolzani@flowgate.net>
2024-12-02 12:39:49 -0300

    fixing removed well-known directory

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker stop juiceshop || true
-docker pull vbolzani/juice-shop-dso:latest
+docker rm juiceshop || true
+docker pull vbolzanifg/juice-shop-dso:latest
 docker run -d --name juiceshop -p 3000:3000 vbolzanifg/juice-shop-dso:latest
