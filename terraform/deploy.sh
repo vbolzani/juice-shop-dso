e699107058cf1ba4dfd68c8f16ca40cf18964260
vbolzani <vbolzani@flowgate.net>
2024-12-02 11:24:00 -0300

    Push into main

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -2,5 +2,4 @@
 
 docker stop juiceshop || true
 # docker pull vbolzani/juice-shop-dso:latest
-# docker run -d --name juiceshop -p 3000:3000 vbolzani/juice-shop-dso:latest
-docker run -d --name juiceshop -p 3000:3000 bkimminich/juice-shop
+docker run -d --name juiceshop -p 3000:3000 vbolzani/juice-shop-dso:latest
