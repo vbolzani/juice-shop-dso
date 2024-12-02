c085ccfc5e8c547885f2e2510bfaa6fb9d271cb8
vbolzani <vbolzani@flowgate.net>
2024-12-02 12:04:15 -0300

    Fixing deploy script

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker stop juiceshop || true
-# docker pull vbolzani/juice-shop-dso:latest
-docker run -d --name juiceshop -p 3000:3000 vbolzani/juice-shop-dso:latest
+docker pull vbolzani/juice-shop-dso:latest
+docker run -d --name juiceshop -p 3000:3000 vbolzanifg/juice-shop-dso:latest
